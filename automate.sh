b6d0dd8f64677ed1744c14435a995b06f5cbe811
Mikey <mikeykentarocho@gmail.com>
2017-10-21 22:50:52 +0000

    DHCPCD configuration file

--- automate.sh
+++ automate.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-#install git
-#sudo apt-get install git
-
 #install the necessary packages
-#sudo apt-get install dnsmasq hostapd
+sudo apt-get install dnsmasq hostapd
 
-#backup the original config file
-sudo cp /etc/dhcpcd.conf /etc/dhcpcd.orig
+#backup the original config files
+sudo cp /etc/dhcpcd.conf /etc/dhcpcd.old
+sudo cp /etc/network/interfaces /etc/network/interfaces.old
 
 #update the config file
-var=`grep -n "interface" test | head -n 1 | cut -c1-1`
-sed "${var} i denyinterfaces wlan0" test 
+
 
 #update the network/interfaces file to setup static IP
 #assume 192.168.50.1
 
+
 #restart the dhcpcd
-#sudo service dhcpcd restart
+sudo service dhcpcd restart
 
 #reload the wlan0 configuration
 sudo ifdown wlan0; sudo ifup wlan0
 
 #configure the hostapd
 #first create a hostapd
-
+curl -O https://raw.githubusercontent.com/toepump/MaxTuna/master/hostapd.conf
