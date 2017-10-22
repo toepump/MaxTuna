542c525d75492a3538155c49502e742f2e0d9ab9
Mikey Kentaro <mikeykentarocho@gmail.com>
2017-10-22 02:28:46 -0700

    border

--- public/index.php
+++ public/index.php
@@ -34,7 +34,7 @@
     </div>
 
     <div class = "jumbotron">
-        <img style = "height:100px; width:100px;" src="assets/images/desktop.png"/>
+        <img style = "height:100px; width:100px; border: solid black 1px" src="assets/images/desktop.png"/>
     </div>
 
     <div style = "background-color: #BBE1E5;" class="jumbotron">
