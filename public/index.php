ac79c90a10b1649e6bef14f28381351f187d8a7c
Mikey Kentaro <mikeykentarocho@gmail.com>
2017-10-22 02:18:29 -0700

    images

--- public/index.php
+++ public/index.php
@@ -33,6 +33,10 @@
         </form>
     </div>
 
+    <div class = "jumbotron">
+        <img src="assets/images/desktop.png"/>
+    </div>
+
     <div class="progress">
         <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
     </div>
