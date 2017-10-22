171d9596002dd38ae618265aef073831318b302a
telwell <me@trevorelwell.com>
2017-10-21 18:03:33 -0700

    testing

--- public/index.php
+++ public/index.php
@@ -1,15 +1,10 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <title>Hello, world!</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-</head>
+<?php include 'header.php';?>
 <body>
+    <pre>
+      <?=
+        var_export($eeids, 42);
+      ?>
+    </pre>
     <div class="jumbotron">
         <h1 class="display-3">MaxTuna</h1>
         <p class="lead">Connect quickly, and securely to your public networks.</p>
