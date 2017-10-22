5b1f43e84feb168b83f4a5bc3d417ba7d665e4e3
telwell <me@trevorelwell.com>
2017-10-21 21:54:05 -0700

    check if up

--- public/connected.php
+++ public/connected.php
@@ -1,7 +1,8 @@
 <?php
   // If we can ping google.com then we return true. Otherwise false.
-  $result = trim(shell_exec('curl google.com 2>&1 | grep -c "Failed to connect"'));
-  $connected = ($result == "0" ? true : false);
+  $t1 = trim(shell_exec('curl google.com 2>&1 | grep -c "Failed to connect"'));
+  $t2 = trim(shell_exec('curl google.com 2>&1 | grep -c "Could not resolve"'));
+  $connected = (($t1 == "0" || $t2 == "0" ) ? true : false);
   header('Content-type: application/json');
   echo json_encode(array('response' => $connected));
 ?>
