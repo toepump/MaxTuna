4e4a7cccdd096ebf612b4c7e6f1a2c684769ede9
telwell <me@trevorelwell.com>
2017-10-21 21:25:44 -0700

    check if up

--- public/connected.php
+++ public/connected.php
@@ -1,5 +1,6 @@
 <?php
-  $connected = shell_exec('curl google.com 2>&1 | grep -c "Could not resolve"');
+  $result = trim(shell_exec('curl google.com 2>&1 | grep -c "Could not resolve"'));
+  $connected = ($result == "0" ? true : false);
   header('Content-type: application/json');
   echo json_encode(array('response' => $connected));
 ?>
