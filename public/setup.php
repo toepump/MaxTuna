9d193b5859a5fb2f1d329817fbed9de061e64e4b
telwell <me@trevorelwell.com>
2017-10-21 20:18:29 -0700

    test

--- public/setup.php
+++ public/setup.php
@@ -8,15 +8,15 @@
   unlink($filename);
 
   // CREATE NEW FILE
-  $buffer = 'country=GB\n';
-  $buffer .= 'ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\n';
-  $buffer .= 'update_config=1\n';
-  $buffer .= '\n';
-  $buffer .= 'network={\n';
-  $buffer .= 'ssid="'. $network . '"\n';
-  $buffer .= 'psk="'. $pass . '"\n';
-  $buffer .= 'key_mgmt=WPA-PSK\n';
-  $buffer .= '}';
+  $buffer = "country=GB\n";
+  $buffer .= "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\n";
+  $buffer .= "update_config=1\n";
+  $buffer .= "\n";
+  $buffer .= "network={\n";
+  $buffer .= "ssid=\"". $network . "\"\n";
+  $buffer .= "psk=\"". $pass . "\"\n";
+  $buffer .= "key_mgmt=WPA-PSK\n";
+  $buffer .= "}";
 
   file_put_contents ($filename , $buffer);
 
