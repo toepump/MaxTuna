5b5920dbd4a6c76869d043b901a05f5a52179493
telwell <me@trevorelwell.com>
2017-10-22 08:35:21 -0700

    test

--- public/index.php
+++ public/index.php
@@ -1,48 +1,48 @@
 <?php include 'header.php';?>
   <body class="container-fluid">
-    <div class="jumbotron">
-      <h1 class="display-3">Connect Buddha</h1>
+
+    <!-- Fullpage row section -->
+    <section class="row justify-content-center">
+
+      <h1>Connect Buddha</h1>
       <p class="lead">Connect quickly, and securely to public networks.</p>
       <hr class="my-4">
-      <section class="row justify-content-center">
 
-        <!-- Connect icon -->
-        <div class="connected">
-          <i class="fa fa-spinner fa-pulse fa-fw" aria-hidden="true"></i><span class="text-warning">Connecting</span>
-        </div>
-        <!-- END connect icon -->
-        
-        <div class="col-8">
+      <!-- Connect icon -->
+      <div class="connected">
+        <i class="fa fa-spinner fa-pulse fa-fw" aria-hidden="true"></i><span class="text-warning">Connecting</span>
+      </div>
+      <!-- END connect icon -->
 
-          <!-- BEGIN form area -->
-          <p>Available Networks: </p>
+      <div class="col-8">
 
-          <form method="GET" action="setup.php">
-            <div class="form-group">
-              <select name="network" class="form-control">
-                <?php foreach ($ids as $id): ?>
-                  <?php
-                  $id = ltrim(trim($id), "ESSID:\"");
-                  $id = rtrim($id, "\"");
-                  ?>
-                  <option value="<?php echo $id; ?>"><?php echo $id; ?></option>
-                <?php endforeach; ?>
-              </select>
-            </div>
+        <!-- BEGIN form area -->
+        <p>Available Networks: </p>
 
-            <div class="form-group">
-              <input class="form-control" type="password" name="password" placeholder="Enter Wifi Password">
-            </div>
+        <form method="GET" action="setup.php">
+          <div class="form-group">
+            <select name="network" class="form-control">
+              <?php foreach ($ids as $id): ?>
+                <?php
+                $id = ltrim(trim($id), "ESSID:\"");
+                $id = rtrim($id, "\"");
+                ?>
+                <option value="<?php echo $id; ?>"><?php echo $id; ?></option>
+              <?php endforeach; ?>
+            </select>
+          </div>
 
-            <div class="form-group">
-              <button type="submit" class="btn btn-primary btn-lg" role="button">
-                Connect
-              </a>
-            </div>
-          </form>
-          <!-- END form -->
+          <div class="form-group">
+            <input class="form-control" type="password" name="password" placeholder="Enter Wifi Password">
+          </div>
 
-        </div>
+          <div class="form-group">
+            <button type="submit" class="btn btn-primary btn-lg" role="button">
+              Connect
+            </a>
+          </div>
+        </form>
+        <!-- END form -->
 
       </section>
     </div>
