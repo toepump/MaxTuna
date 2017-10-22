818c15bcbd324206a0ea39d639912926b6e8fa27
Mikey Kentaro <mikeykentarocho@gmail.com>
2017-10-22 02:44:51 -0700

    better icons

--- public/index.php
+++ public/index.php
@@ -35,9 +35,9 @@
 
     <div class = "jumbotron">
         <div style="border:solid black 1px;">
-            <img style = "padding: 30px; height:100px; width:100px; background-color: white; border-radius: 50%; margin-left: 20%;" src="assets/images/desktop.png"/>
-            <img style = "padding: 30px; height:100px; width:100px; background-color: white; border-radius: 50%; margin-left: 20%;" src="assets/images/router.png"/>
-            <img style = "padding: 30px; height:100px; width:100px; background-color: white; border-radius: 50%; margin-left: 20%;" src="assets/images/server.png"/>
+            <img style = "padding: 20px; height:100px; width:100px; background-color: white; border-radius: 50%; margin-left: 20%;" src="assets/images/desktop.png"/>
+            <img style = "padding: 20px; height:100px; width:100px; background-color: white; border-radius: 50%; margin-left: 20%;" src="assets/images/router.png"/>
+            <img style = "padding: 20px; height:100px; width:100px; background-color: white; border-radius: 50%; margin-left: 20%;" src="assets/images/server.png"/>
         </div>
     </div>
 
