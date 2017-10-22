d085ea94cf514f2be731b0f1493e684f7ff488c9
telwell <me@trevorelwell.com>
2017-10-21 17:09:47 -0700

    initial public comit

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Max Tuna</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  </head>
+  <body>
+    <h2>Hey Hunny!</h2>
+  </body>
+
+  <script src="assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</html>
