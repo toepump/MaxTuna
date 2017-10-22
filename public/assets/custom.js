56f1705d7ba19e585c481b191a0dcebee30063be
telwell <me@trevorelwell.com>
2017-10-21 21:58:15 -0700

    check if up

--- public/assets/custom.js
+++ public/assets/custom.js
@@ -5,11 +5,14 @@ $(document).ready(function(){
     // data: data,
     // dataType: dataType
     success: function(data, text, xhr){
-      $($('.connected').get(0)).html('<span class="text-success">Connected!</span>');
+      if(text.response == "true"){
+        $($('.connected').get(0)).html('<span class="text-success">Connected!</span>');
+      } else {
+        $($('.connected').get(0)).html('<span class="text-error">Not Connected</span>');
+      }
     },
     error: function(xhr, text, error){
       console.log(text);
-      $($('.connected').get(0)).html('<span class="text-error">Not Connected</span>');
     }
   });
 });
--- public/connected.php
+++ public/connected.php
@@ -2,7 +2,7 @@
   // If we can ping google.com then we return true. Otherwise false.
   $t1 = trim(shell_exec('curl google.com 2>&1 | grep -c "Failed to connect"'));
   $t2 = trim(shell_exec('curl google.com 2>&1 | grep -c "Could not resolve"'));
-  $connected = (($t1 == "0" || $t2 == "0" ) ? true : false);
+  $connected = (($t1 == "1" || $t2 == "1" ) ? false : true);
   header('Content-type: application/json');
   echo json_encode(array('response' => $connected));
 ?>
