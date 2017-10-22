20b9cb5ced22ec1deb91ea009c8764b0784d884a
telwell <me@trevorelwell.com>
2017-10-22 08:31:56 -0700

    test

--- public/index.php
+++ public/index.php
@@ -1,21 +1,27 @@
 <?php include 'header.php';?>
-<body>
+  <body class="container-fluid">
     <div class="jumbotron">
-        <h1 class="display-3">MaxTuna</h1>
-        <p class="lead">Connect quickly, and securely to your public networks.</p>
-        <hr class="my-4">
-        <p>Available Network: </p>
-        <form method="GET" action="setup.php">
+      <h1 class="display-3">Connect Buddha</h1>
+      <p class="lead">Connect quickly, and securely to public networks.</p>
+      <hr class="my-4">
+      <section class="row justify-content-center">
+
+        <div class="col-8">
+
+          <!-- BEGIN form area -->
+          <p>Available Networks: </p>
+
+          <form method="GET" action="setup.php">
             <div class="form-group">
-                <select name="network" class="form-control">
-                    <?php foreach ($ids as $id): ?>
-                        <?php
-                        $id = ltrim(trim($id), "ESSID:\"");
-                        $id = rtrim($id, "\"");
-                        ?>
-                        <option value="<?php echo $id; ?>"><?php echo $id; ?></option>
-                    <?php endforeach; ?>
-                </select>
+              <select name="network" class="form-control">
+                <?php foreach ($ids as $id): ?>
+                  <?php
+                  $id = ltrim(trim($id), "ESSID:\"");
+                  $id = rtrim($id, "\"");
+                  ?>
+                  <option value="<?php echo $id; ?>"><?php echo $id; ?></option>
+                <?php endforeach; ?>
+              </select>
             </div>
 
             <div class="form-group">
@@ -23,51 +29,22 @@
             </div>
 
             <div class="form-group">
-                <button type="submit" class="btn btn-primary btn-lg" role="button">
-                    Connect
-                </a>
+              <button type="submit" class="btn btn-primary btn-lg" role="button">
+                Connect
+              </a>
             </div>
-        </form>
-    </div>
+          </form>
+          <!-- END form -->
 
-    <div class="connected">
-      <i class="fa fa-spinner fa-pulse fa-fw" aria-hidden="true"></i><span class="text-warning">Connecting</span>
-    </div>
-
-    <div class = "jumbotron">
-        <div>
-            <img style = "padding: 20px; height:150px; width:150px; border-radius:50%; background-color: white;  margin-left: 20%;" src="assets/images/desktop.png"/>
-            <img style = "padding: 20px; height:150px; width:150px; border-radius:50%; background-color: white;  margin-left: 20%;" src="assets/images/router.png"/>
-            <img style = "padding: 20px; height:150px; width:150px; border-radius:50%; background-color: white;  margin-left: 20%;" src="assets/images/server.png"/>
         </div>
+
+      </section>
     </div>
 
-    <div style = "background-color: #BBE1E5;" class="jumbotron">
-        <h1 class="display-3">Connected Users:</h1>
-        <p class="lead">Connect quickly, and securely to your public networks.</p>
-        <hr class="my-4">
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <p class="btn-group">
-            <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Learn more
-            </button>
-            <select class="dropdown-menu">
-                <?php foreach ($ids as $id): ?>
-                    <?php
-                    $id = ltrim(trim($id), "ESSID:\"");
-                    $id = rtrim($id, "\"");
-                    ?>
-                    <option class="dropdown-item" value="<?php echo $id; ?>"><?php echo $id; ?></option>
-                <?php endforeach; ?>
-            </select>
-        </p>
-    </div>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <!-- js -->
     <script src="assets/jquery/jquery.min.js"></script>
     <script src="assets/popper/popper.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="assets/custom.js"></script>
-</body>
+  </body>
 </html>
