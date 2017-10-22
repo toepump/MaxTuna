d817653a0828aff06750aba7a7957c124dd1995e
telwell <me@trevorelwell.com>
2017-10-21 20:37:36 -0700

    fixing files and trying ajax

---
+++ public/assets/custom.js
@@ -0,0 +1,14 @@
+$document.ready(function(){
+  $.ajax({
+    url: 'connected.php',
+    method: 'GET',
+    // data: data,
+    // dataType: dataType
+    success: function(data, text, xhr){
+      console.log(text);
+    },
+    error: function(xhr, text, error){
+      console.log(text);
+    }
+  });
+});
